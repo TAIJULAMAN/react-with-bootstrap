95e94a0a57400355596204e94ccfd60e63119ecb
TAIJULAMAN <taijulaman8@gmail.com>
2023-07-27 01:43:40 +0600

    basic use of bootstrap in react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CardGroup from './components/CardGroup/CardGroup';
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+     <h2>hi!!!!!!!!this is bootstrap</h2>
+     <CardGroup></CardGroup>
+    </div>
+  );
+}
+
+export default App;
